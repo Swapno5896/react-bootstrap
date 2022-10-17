92d1a650c572aaa6f27413634db7887c7b67e55a
swaono5896 <swapnom73@gmail.com>
2022-10-17 20:29:58 +0600

    57.6

---
+++ Authnetication/auth-57/src/App.js
@@ -0,0 +1,132 @@
+import './App.css';
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import app from './firebase,init';
+// react bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+function App() {
+  const auth = getAuth(app);
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [regestered, setRegestered] = useState(false);
+
+  // set error to show
+  const [err, setErr] = useState('')
+
+  // for validation
+  const [validated, setValidated] = useState(false);
+
+  // hadel input fild
+  const handelBlutEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handelBlutPass = (event) => {
+    setPass(event.target.value)
+  }
+
+  //  handel restered or login
+  const handelRegesterChange = event => {
+    setRegestered(event.target.checked);
+  }
+  // handel submit button
+  const handelSumbit = (event) => {
+    event.preventDefault();
+    // bootstrap validation
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return
+    }
+    // checking password wheather container 6 character and a number or not
+    if (!/^(?=.{6,20}$)\D*\d/.test(pass)) {
+      setErr('Password should be at least 6 character and should contain one number')
+      return
+    }
+    setValidated(true);
+    setErr('')
+    // creation user
+    console.log(email, pass);
+    event.preventDefault();
+
+    // if regestesred then we will login
+    if (regestered) {
+      signInWithEmailAndPassword(auth, email, pass)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user)
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorMessage)
+        });
+
+
+      //
+
+    }
+
+    // if not regestesred then we will regestered
+    else {
+      createUserWithEmailAndPassword(auth, email, pass)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user);
+
+          //  clean form
+          setPass('');
+          setEmail('');
+          // ...
+        })
+        .catch((error) => {
+
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErr(errorMessage);
+          // ..
+        });
+    }
+
+  }
+
+  return (
+    <div className='w-50 mx-auto mt-5'>
+      <h3 className='text-primary'>{regestered ? 'LogIn' : 'Regester'} Hear</h3>
+      <Form noValidate validated={validated} onSubmit={handelSumbit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control required onBlur={handelBlutEmail} type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Email.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control required onBlur={handelBlutPass} type="password" placeholder="Password" />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Password.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group onChange={handelRegesterChange} className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Already Regestered ?" />
+        </Form.Group>
+        <p className='text-danger'>{err}</p>
+        <Button variant="primary" type="submit">
+          {regestered ? 'LogIn' : 'Regester'}
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default App;
---
+++ Authnetication/auth-57/src/firebase,init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAedAOyq2faSLJVGI8_nyPk1N2Fy3ctv6o",
+    authDomain: "auth-57.firebaseapp.com",
+    projectId: "auth-57",
+    storageBucket: "auth-57.appspot.com",
+    messagingSenderId: "443003331336",
+    appId: "1:443003331336:web:c2df76df0b0313cda202bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
